4eb58af4191c3f2d8c9b89605654f2c38521c67b
Роман Хамидулин <romanhamidulin@ya.ru>
2025-11-19 09:44:48 +0500

    Реализация DTO+правка pom

---
+++ stat/stat-dto/src/main/java/ru/practicum/dto/ViewStatsDto.java
@@ -0,0 +1,18 @@
+﻿package ru.practicum.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ViewStatsDto {
+    @NotBlank
+    private String app;
+    @NotBlank
+    private String uri;
+    @NotBlank
+    private Long hits;
+}
