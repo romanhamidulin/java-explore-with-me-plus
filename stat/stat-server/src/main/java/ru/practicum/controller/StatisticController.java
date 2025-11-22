61f6e022e294907b0b9019b60d5ca0aaaaba296d
Роман Хамидулин <romanhamidulin@ya.ru>
2025-11-22 15:31:13 +0500

    убрал неиспользуемый импорт

--- stat/stat-server/src/main/java/ru/practicum/controller/StatisticController.java
+++ stat/stat-server/src/main/java/ru/practicum/controller/StatisticController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.service.StatisticService;
 import ru.practicum.dto.EndpointHitDto;
