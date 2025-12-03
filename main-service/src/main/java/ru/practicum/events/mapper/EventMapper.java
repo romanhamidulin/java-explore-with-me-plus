79819b19f0a4c29d109fb626c23217b1c528b7b8
Роман Хамидулин <romanhamidulin@ya.ru>
2025-12-03 10:08:27 +0500

    остаось 5 тестов, ошибки в event

--- main-service/src/main/java/ru/practicum/events/mapper/EventMapper.java
+++ main-service/src/main/java/ru/practicum/events/mapper/EventMapper.java
@@ -8,13 +8,14 @@ import ru.practicum.events.dto.EventDto;
 import ru.practicum.events.dto.EventShortDto;
 import ru.practicum.events.dto.LocationDto;
 import ru.practicum.events.model.Event;
+import ru.practicum.events.model.EventState;
 import ru.practicum.user.dto.UserShortDto;
 import ru.practicum.user.mapper.UserMapper;
 
 @UtilityClass
 public class EventMapper {
     public EventDto mapToDto(Event event, Long confirmedRequest, Long views) {
-        return EventDto.builder()
+        EventDto.EventDtoBuilder builder = EventDto.builder()
                 .id(event.getId())
                 .title(event.getTitle())
                 .annotation(event.getAnnotation())
@@ -35,28 +36,37 @@ public class EventMapper {
                         .build())
                 .eventDate(event.getEventDate())
                 .createdOn(event.getCreatedOn())
-                .publishedOn(event.getPublishedOn())
-                .build();
+                .publishedOn(event.getPublishedOn());
+
+        if (event.getCategory() != null) {
+            builder.category(CategoryMapper.mapToDto(event.getCategory()));
+        }
+
+        return builder.build();
     }
 
     public static EventShortDto mapToShortDto(Event event, Long confirmedRequest, Long views) {
-        return EventShortDto.builder()
+        EventShortDto.EventShortDtoBuilder builder = EventShortDto.builder()
                 .id(event.getId())
                 .title(event.getTitle())
                 .annotation(event.getAnnotation())
                 .confirmedRequests(confirmedRequest != null ? confirmedRequest : 0L)
                 .views(views != null ? views : 0L)
                 .paid(event.getPaid())
-                .category(CategoryDto.builder()
-                        .id(event.getCategory().getId())
-                        .name(event.getCategory().getName())
-                        .build())
                 .initiator(UserShortDto.builder()
                         .id(event.getInitiator().getId())
                         .name(event.getInitiator().getName())
                         .build())
-                .eventDate(event.getEventDate())
-                .build();
+                .eventDate(event.getEventDate());
+
+        if (event.getCategory() != null) {
+            builder.category(CategoryDto.builder()
+                    .id(event.getCategory().getId())
+                    .name(event.getCategory().getName())
+                    .build());
+        }
+
+        return builder.build();
     }
 
     public Event toEvent(EventCreateDto dto) {
@@ -72,7 +82,7 @@ public class EventMapper {
     }
 
     public EventShortDto toEventShortDto(Event event) {
-        return EventShortDto.builder()
+        EventShortDto.EventShortDtoBuilder builder = EventShortDto.builder()
                 .id(event.getId())
                 .title(event.getTitle())
                 .annotation(event.getAnnotation())
@@ -80,13 +90,17 @@ public class EventMapper {
                 .views(0L)
                 .eventDate(event.getEventDate())
                 .paid(event.getPaid())
-                .category(CategoryMapper.mapToDto(event.getCategory()))
-                .initiator(UserMapper.toUserShortDto(event.getInitiator()))
-                .build();
+                .initiator(UserMapper.toUserShortDto(event.getInitiator()));
+
+        if (event.getCategory() != null) {
+            builder.category(CategoryMapper.mapToDto(event.getCategory()));
+        }
+
+        return builder.build();
     }
 
     public EventDto toEventDto(Event event) {
-        return EventDto.builder()
+        EventDto.EventDtoBuilder builder = EventDto.builder()
                 .id(event.getId())
                 .title(event.getTitle())
                 .annotation(event.getAnnotation())
@@ -97,12 +111,16 @@ public class EventMapper {
                 .paid(event.getPaid())
                 .requestModeration(event.getRequestModeration())
                 .participantLimit(event.getParticipantLimit())
-                .category(CategoryMapper.mapToDto(event.getCategory()))
                 .location(LocationMapper.toLocationDto(event.getLocation()))
                 .initiator(UserMapper.toUserShortDto(event.getInitiator()))
                 .state(event.getState())
                 .publishedOn(event.getPublishedOn())
-                .createdOn(event.getCreatedOn())
-                .build();
+                .createdOn(event.getCreatedOn());
+
+        if (event.getCategory() != null) {
+            builder.category(CategoryMapper.mapToDto(event.getCategory()));
+        }
+
+        return builder.build();
     }
 }
